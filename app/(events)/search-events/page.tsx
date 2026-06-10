b7506a1a2071e58d1d54a44388bc58c88d528a7e
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-10 19:26:47 +0300

    feat: implement Search Events page with header and error boundary

--- app/(events)/search-events/page.tsx
+++ app/(events)/search-events/page.tsx
@@ -1,3 +1,19 @@
-export default async function Page() {
-  return <></>
+import ErrorBoundry from '@/app/components/error/ErrorBoundry'
+import { EventsListSearch } from '@/app/components/search/EventsListSearch'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Search Events',
+}
+
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-3xl text-center my-10">Search list</h1>
+
+      <ErrorBoundry>
+        <EventsListSearch />
+      </ErrorBoundry>
+    </>
+  )
 }
