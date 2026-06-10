19fd5dd657d3b614cde9ac6e9ce11ea083c03cbd
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-10 19:34:54 +0300

    feat: enhance Search Events page to handle search parameters and redirect if not provided

--- app/(events)/search-events/page.tsx
+++ app/(events)/search-events/page.tsx
@@ -1,12 +1,23 @@
 import ErrorBoundry from '@/app/components/error/ErrorBoundry'
 import { EventsListSearch } from '@/app/components/search/EventsListSearch'
 import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Search Events',
 }
 
-export default function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ search: string }>
+}) {
+  const { search } = await searchParams
+
+  if (!search) {
+    redirect('/')
+  }
+
   return (
     <>
       <h1 className="text-3xl text-center my-10">Search list</h1>
