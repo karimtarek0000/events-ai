2ff5169a52fa257ba49bb3427c72550f65561159
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-11 19:47:08 +0300

    feat: implement getUserPlan function to fetch user subscription details

---
+++ utils/index.server.ts
@@ -0,0 +1,27 @@
+'server-only'
+
+import { auth, clerkClient } from '@clerk/nextjs/server'
+
+export async function getUserPlan() {
+  const { userId } = await auth.protect()
+  const client = await clerkClient()
+
+  try {
+    // Fetch the active subscription details from Clerk
+    const subscription = await client.billing.getUserBillingSubscription(userId)
+    // Safely check if a subscription and active items exist
+    const activeItem = subscription?.subscriptionItems?.[0]
+    if (!activeItem) {
+      return { planId: null, slug: null, name: 'Free/No Plan' }
+    }
+    // Extract details from the nested plan object
+    return {
+      // planId: activeItem.planId, // e.g., 'cplan_3EpXjfa...'
+      // slug: activeItem.plan?.slug || null, // e.g., 'pro-plan'
+      name: activeItem.plan?.name.toLowerCase(), // e.g., 'Pro Plan'
+      // status: subscription.status, // e.g., 'active'
+    }
+  } catch {
+    return { name: 'Free/No Plan' }
+  }
+}
