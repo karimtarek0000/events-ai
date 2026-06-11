e5c8b9cb8d5712c6aa59008485dda7c0ccf96af7
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-11 05:39:17 +0300

    feat: implement event page with metadata generation and event details display

--- app/(events)/events/[id]/page.tsx
+++ app/(events)/events/[id]/page.tsx
@@ -1,3 +1,37 @@
-export default async function Page() {
-  return <h1>testing</h1>
+import ErrorBoundry from '@/app/components/error/ErrorBoundry'
+import EventDetails from '@/app/components/event/EventDetails'
+import EventsSkeleton from '@/app/components/skeleton/EventsSkeleton'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { fetchQuery, preloadQuery } from 'convex/nextjs'
+import type { Metadata } from 'next'
+import { Suspense } from 'react'
+
+interface PageProps {
+  params: Promise<{ id: Id<'events'> }>
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { id } = await params
+  const event = await fetchQuery(api.events.getEvent, { eventId: id })
+
+  if (!event) return { title: 'Event Not Found' }
+
+  return {
+    title: event.title,
+    description: event.description,
+  }
+}
+
+export default async function Page({ params }: PageProps) {
+  const { id } = await params
+  const event = await preloadQuery(api.events.getEvent, { eventId: id })
+
+  return (
+    <ErrorBoundry>
+      <Suspense fallback={<EventsSkeleton />}>
+        <EventDetails preloadedEvents={event} />
+      </Suspense>
+    </ErrorBoundry>
+  )
 }
