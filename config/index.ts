fb933bf07806e02452283fd6eeff41d30dc128ac
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-12 19:08:50 +0300

    feat: add 'free' plan option and define plan limits in configuration

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,14 @@
-export type Plans = 'starter' | 'pro' | 'max'
+export type Plans = 'starter' | 'pro' | 'max' | 'free'
 export const PLANS = {
+  free: 'free',
   starter: 'starter',
   pro: 'pro',
   max: 'max',
 } as const
+
+export const PLAN_LIMITS: Record<Plans, number> = {
+  free: 1,
+  starter: 3,
+  pro: 10,
+  max: Infinity,
+}
