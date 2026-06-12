1be93dd52ca558cea7d7e16954cce5111aa36455
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-12 19:35:38 +0300

    refactor: update import path for EventsList component in FeaturedCarousel

--- app/components/features/FeaturedCarouesel.tsx
+++ app/components/features/FeaturedCarouesel.tsx
@@ -1,6 +1,6 @@
 import { api } from '@/convex/_generated/api'
 import { preloadQuery } from 'convex/nextjs'
-import EventsList from './EventsList'
+import EventsList from '../event/EventsList'
 
 export default async function FeaturedCarouesel() {
   const events = await preloadQuery(api.events.getFeaturedEvents, { limit: 4 })
