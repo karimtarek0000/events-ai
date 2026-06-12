ffca078b675b34d82f3936aca3008d099259ca9c
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-12 19:35:51 +0300

    refactor: update import path for EventsList component in NearbyEvents

--- app/components/features/NearbyEvents.tsx
+++ app/components/features/NearbyEvents.tsx
@@ -1,6 +1,6 @@
 import { api } from '@/convex/_generated/api'
 import { preloadQuery } from 'convex/nextjs'
-import EventsList from './EventsList'
+import EventsList from '../event/EventsList'
 
 export default async function NearbyEvents() {
   const events = await preloadQuery(api.events.getEventsByLocation, { limit: 4 })
