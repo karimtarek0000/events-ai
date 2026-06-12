7e8710f71d5301ac0c0610ff21e56bf3d831dbc1
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-12 19:35:21 +0300

    feat: implement EventsList component to display events with optional slider functionality

---
+++ app/components/event/EventsList.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { api } from '@/convex/_generated/api'
+import { Preloaded, usePreloadedQuery } from 'convex/react'
+import NotFound from '../notFound/NotFound'
+import EventCard from './EventCard'
+import FeaturedCaroueselCard from './FeaturedCaroueselCard'
+
+interface EventsListProps {
+  preloadedEvents:
+    | Preloaded<typeof api.events.getEventsByLocation>
+    | Preloaded<typeof api.events.getFeaturedEvents>
+    | Preloaded<typeof api.events.getPopularEvents>
+    | Preloaded<typeof api.events.getEventsByLocation>
+  slider?: boolean
+}
+
+const EventsList = ({ preloadedEvents, slider }: EventsListProps) => {
+  const events = usePreloadedQuery(preloadedEvents)
+
+  if (slider) {
+    return (
+      <NotFound records={events}>
+        <FeaturedCaroueselCard events={events} />
+      </NotFound>
+    )
+  }
+
+  return (
+    <NotFound records={events}>
+      <section className="space-y-6 w-full h-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {events?.map(event => (
+            <EventCard key={event._id} event={event} />
+          ))}
+        </div>
+      </section>
+    </NotFound>
+  )
+}
+
+export default EventsList
