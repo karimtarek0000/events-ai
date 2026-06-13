9c51c715f4869920d5e4b70e3e55d3d17c0ccda2
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-13 23:24:16 +0300

    feat: introduce EventForm component for streamlined event creation and editing with improved form handling

---
+++ app/components/event/EventForm.tsx
@@ -0,0 +1,134 @@
+'use client'
+
+import { Badge } from '@/components/ui/badge'
+import { useSearchParams } from 'next/navigation'
+import {
+  api,
+  BasicInfoSection,
+  Button,
+  CapacitySection,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CustomizationSection,
+  DateTimeSection,
+  errorMessageHandle,
+  EventFormValues,
+  eventSchema,
+  FormInput,
+  FormOutput,
+  FormProvider,
+  LocationSection,
+  toast,
+  useForm,
+  useMutation,
+  useRouter,
+  useTransition,
+  zodResolver,
+} from '.'
+
+const initialValues = {
+  category: '',
+  ticketType: '',
+  startDate: '',
+  endDate: '',
+  locationType: '',
+}
+
+export default function EventForm({ plan }: { plan: string }) {
+  const router = useRouter()
+
+  const searchParams = useSearchParams()
+  const raw = searchParams.get('edit_event')
+  const event = raw ? JSON.parse(decodeURIComponent(raw)) : null
+
+  const createEvent = useMutation(api.events.createAndUpdate)
+
+  const [isPending, startTransition] = useTransition()
+
+  const form = useForm<FormInput, unknown, FormOutput>({
+    resolver: zodResolver(eventSchema),
+    shouldUnregister: true,
+    mode: 'onChange',
+    values: event ?? initialValues,
+  })
+
+  const onSubmit = async (data: EventFormValues) => {
+    startTransition(async () => {
+      try {
+        await createEvent({
+          ...data,
+          eventId: event?._id ?? undefined,
+          category: data.category ?? '',
+          startDate: data.startDate.getTime(),
+          endDate: data.endDate.getTime(),
+          tags: data.tags || [],
+          ticketPrice: data.ticketType === 'paid' ? data.ticketPrice : undefined,
+          venue: data.venue || undefined,
+          address: data.address || undefined,
+          state: data.state || undefined,
+          themeColor: data.themeColor,
+        })
+
+        router.push('/')
+        toast.success(`${event?._id ? 'Edit and event' : 'Create a new event'} successfully`)
+      } catch (err) {
+        const errorMessage = errorMessageHandle(err)
+        toast.error(errorMessage)
+      }
+    })
+  }
+
+  return (
+    <div className="max-w-4xl mx-auto px-4 py-5">
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex justify-between capitalize">
+            <span>{event?._id ? 'edit event' : 'create new event'}</span>
+            <Badge className="capitalize font-bold text-[1rem] p-2">{plan}</Badge>
+          </CardTitle>
+
+          <CardDescription>
+            {event?._id
+              ? 'Edit in the details below'
+              : 'Fill in the details below to create your event'}
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <BasicInfoSection />
+              <hr />
+              <DateTimeSection />
+              <hr />
+              <LocationSection />
+              <hr />
+              <CapacitySection />
+              <hr />
+              <CustomizationSection />
+
+              <div className="flex gap-4 pt-4">
+                <Button type="submit" disabled={isPending} className="flex-1 capitalize">
+                  {isPending
+                    ? `${event?._id ? 'editing event' : 'creating event'}...`
+                    : `${event?._id ? 'edit event' : 'create event'}`}
+                </Button>
+                <Button
+                  type="button"
+                  variant="outline"
+                  onClick={() => router.push('/explore')}
+                  disabled={isPending}
+                >
+                  Cancel
+                </Button>
+              </div>
+            </form>
+          </FormProvider>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
