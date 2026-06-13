dd844026ab711186d1271d374cac7d7204b630c6
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-13 23:24:07 +0300

    refactor: replace CreateEventForm with EventForm in create-event page for improved event handling

--- app/(events)/create-event/page.tsx
+++ app/(events)/create-event/page.tsx
@@ -1,14 +1,14 @@
 import { Plans } from '@/config'
 import { getUserPlan } from '@/utils/index.server'
-import CreateEventForm from '../../components/event/CreateEventForm'
 import ExceedLimit from '../../components/ExceedLimit'
+import EventForm from '../../components/event/EventForm'
 
 export default async function Page() {
   const plan = await getUserPlan()
 
   return (
     <ExceedLimit plan={plan.name as Plans}>
-      <CreateEventForm plan={plan.name!} />
+      <EventForm plan={plan.name!} />
     </ExceedLimit>
   )
 }
