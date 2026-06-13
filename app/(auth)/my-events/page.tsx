5c13321f97b1c86d4eca1ccc2a0b70e79ca8f9b1
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-13 19:23:25 +0300

    feat: add metadata for My Events page to improve SEO and page title

--- app/(auth)/my-events/page.tsx
+++ app/(auth)/my-events/page.tsx
@@ -1,6 +1,11 @@
 import EventListMyEvent from '@/app/components/event/EventListMyEvents'
 import { api } from '@/convex/_generated/api'
 import { preloadQuery } from 'convex/nextjs'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'My Events',
+}
 
 export default async function Page() {
   const events = await preloadQuery(api.events.getMyEvents, {})
