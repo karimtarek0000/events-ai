a61e11ae96193e342c87bcdca5f7158451157fd9
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-14 16:30:50 +0300

    style: update heading in NearbyEvents component for improved consistency

--- app/components/features/NearbyEvents.tsx
+++ app/components/features/NearbyEvents.tsx
@@ -7,7 +7,7 @@ export default async function NearbyEvents() {
 
   return (
     <>
-      <h2 className="text-2xl font-bold my-8">⭐ Nearby Events</h2>
+      <h2 className="heading-section">Nearby Events</h2>
       <EventsList preloadedEvents={events} />
     </>
   )
