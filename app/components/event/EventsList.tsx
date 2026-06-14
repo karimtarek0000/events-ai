b57b7009e2bfa089969c48e98cbfb48730af212f
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-14 04:24:21 +0300

    refactor: update EventCard props in EventsList to enable detailed display options for tags and description

--- app/components/event/EventsList.tsx
+++ app/components/event/EventsList.tsx
@@ -31,7 +31,7 @@ const EventsList = ({ preloadedEvents, slider }: EventsListProps) => {
       <section className="space-y-6 w-full h-full">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {events?.map(event => (
-            <EventCard key={event._id} event={event} />
+            <EventCard key={event._id} event={event} isShow={{ tags: true, description: true }} />
           ))}
         </div>
       </section>
