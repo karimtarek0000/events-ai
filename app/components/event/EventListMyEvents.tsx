677eeb0c8d89273b3cc23afe57531a75345dfad2
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-14 04:24:17 +0300

    refactor: update EventCard props in EventListMyEvents to enhance detail display options for tags, description, and edit functionality

--- app/components/event/EventListMyEvents.tsx
+++ app/components/event/EventListMyEvents.tsx
@@ -40,7 +40,7 @@ const EventListMyEvent = ({ preloadedEvents }: EventListMyEventProps) => {
                 checked={ids.includes(event._id)}
                 changeChecked={handleChangeChecked}
               >
-                <EventCard event={event} isShow isEdit />
+                <EventCard event={event} isShow={{ tags: true, description: true, edit: true }} />
               </DeleteEventCheckbox>
             ))}
           </div>
