876449af428d9e48902dac8fdb45f2ba0d3c54e5
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-14 16:35:19 +0300

    feat: enhance ExceedLimit component to conditionally check event creation limit based on edit mode

--- app/components/ExceedLimit.tsx
+++ app/components/ExceedLimit.tsx
@@ -2,6 +2,7 @@
 
 import { PLAN_LIMITS, Plans } from '@/config'
 import { useQuery } from 'convex/react'
+import { useSearchParams } from 'next/navigation'
 import { PropsWithChildren } from 'react'
 import { api } from '../../convex/_generated/api'
 
@@ -12,11 +13,14 @@ interface ExceedLimitProps extends PropsWithChildren {
 const ExceedLimit = ({ plan, children }: ExceedLimitProps) => {
   const eventsCount = useQuery(api.users.getEventsCreatedCount)
 
+  const searchParams = useSearchParams()
+  const isEditEvent = searchParams.get('edit_event')
+
   if (eventsCount === undefined) {
     return <div></div>
   }
 
-  const exceededLimit = eventsCount >= PLAN_LIMITS[plan]
+  const exceededLimit = eventsCount >= PLAN_LIMITS[plan] && !isEditEvent
 
   return exceededLimit ? (
     <p className="text-center">Exceeded the limit of create a new event</p>
