f1b1c96a9fe59df63a8c4cd414d06076bae95019
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-14 04:26:44 +0300

    refactor: enhance EventCard props in EventsListSearch to enable detailed display of description and date

--- app/components/search/EventsListSearch.tsx
+++ app/components/search/EventsListSearch.tsx
@@ -19,7 +19,7 @@ export const EventsListSearch = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {events.map(event => (
-        <EventCard key={event._id} event={event} />
+        <EventCard key={event._id} event={event} isShow={{ description: true, date: true }} />
       ))}
     </div>
   )
