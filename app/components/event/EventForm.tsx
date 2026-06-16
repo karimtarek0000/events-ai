5623be68ae8830b3f3c083c6577e18746f61b282
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-16 04:17:13 +0300

    refactor: streamline EventForm by integrating useEventForm hook for improved form handling and state management

--- app/components/event/EventForm.tsx
+++ app/components/event/EventForm.tsx
@@ -1,10 +1,8 @@
 'use client'
 
-import { revalidateEvent } from '@/actions/event.actions'
-import { isEqual } from 'lodash-es'
-import { useMemo } from 'react'
+import { useEventForm } from '@/hooks/eventForm'
+import router from 'next/router'
 import {
-  api,
   Badge,
   BasicInfoSection,
   Button,
@@ -16,84 +14,12 @@ import {
   CardTitle,
   CustomizationSection,
   DateTimeSection,
-  errorMessageHandle,
-  EventFormValues,
-  eventSchema,
-  FormInput,
-  FormOutput,
   FormProvider,
   LocationSection,
-  toast,
-  useForm,
-  useMutation,
-  useRouter,
-  useSearchParams,
-  useTransition,
-  zodResolver,
 } from '.'
 
-const initialValues = {
-  category: '',
-  ticketType: 'free',
-  startDate: '',
-  endDate: '',
-  locationType: '',
-}
-
 export default function EventForm({ plan }: { plan: string }) {
-  const router = useRouter()
-
-  const searchParams = useSearchParams()
-  const raw = searchParams.get('edit_event')
-  const event = useMemo(() => (raw ? JSON.parse(decodeURIComponent(raw)) : null), [raw])
-
-  const createEvent = useMutation(api.events.createAndUpdate)
-
-  const [isPending, startTransition] = useTransition()
-
-  const form = useForm<FormInput, unknown, FormOutput>({
-    resolver: zodResolver(eventSchema),
-    shouldUnregister: true,
-    mode: 'onChange',
-    values: event ?? initialValues,
-  })
-  const currentValues = form.watch()
-
-  const isDirty = !isEqual(
-    currentValues,
-    Object.fromEntries(Object.keys(currentValues).map(key => [key, event?.[key]])),
-  )
-
-  const onSubmit = async (data: EventFormValues) => {
-    startTransition(async () => {
-      try {
-        await createEvent({
-          ...data,
-          eventId: event?._id ?? undefined,
-          category: data.category ?? '',
-          startDate: data.startDate.getTime(),
-          endDate: data.endDate.getTime(),
-          tags: data.tags || [],
-          ticketPrice: data.ticketType === 'paid' ? data.ticketPrice : undefined,
-          venue: data.venue || undefined,
-          address: data.address || undefined,
-          state: data.state || undefined,
-          themeColor: data.themeColor,
-        })
-
-        router.push('/events')
-        toast.success(
-          `${event?._id ? 'Has been edit an event successfully' : 'Has been created a new event successfully, Check your email'} `,
-        )
-        if (event?._id) {
-          revalidateEvent(event._id)
-        }
-      } catch (err) {
-        const errorMessage = errorMessageHandle(err)
-        toast.error(errorMessage)
-      }
-    })
-  }
+  const { event, form, isDirty, isPending, onSubmit } = useEventForm()
 
   return (
     <div className="max-w-4xl mx-auto px-4 py-5">
@@ -137,7 +63,7 @@ export default function EventForm({ plan }: { plan: string }) {
                 <Button
                   type="button"
                   variant="outline"
-                  onClick={() => router.push('/explore')}
+                  onClick={() => router.back()}
                   disabled={isPending}
                 >
                   Cancel
